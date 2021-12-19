3a583c2c25edf4d0db0e60aee14c33a2d4590c5a
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2021-12-19 00:57:48 -0300

    movendo o objeto para frente

--- PlayerControlPrototype/Assets/scripts/PlayerController.cs
+++ PlayerControlPrototype/Assets/scripts/PlayerController.cs
@@ -6,13 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move o veículo para frente
+        //transform.Translate(0, 0, 1); //usa o componente "transform" e o seu método "Translate" usando as coodernadas x,y,z
+        transform.Translate(Vector3.forward); // outra forma de fazer com que o objeto vá para frente.
+    
     }
 }
