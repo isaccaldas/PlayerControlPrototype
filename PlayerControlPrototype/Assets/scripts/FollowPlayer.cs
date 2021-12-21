d500abd8c357b6437675916295db488a5e7060c0
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2021-12-20 22:22:50 -0300

    Update FollowPlayer

--- PlayerControlPrototype/Assets/scripts/FollowPlayer.cs
+++ PlayerControlPrototype/Assets/scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player; //Declara uma variável do tipo GameObject. 
-
+    private Vector3 offset = new Vector3(0,5,-7):
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {   //offset the camera behind the player by adding  to the player position.
-        transform.position = player.transform.position + new Vector3(0, 5, -7); // Atribui a posição do player a camera.   
+        transform.position = player.transform.position + offset; // Atribui a posição do player a camera.   
     }
 }
